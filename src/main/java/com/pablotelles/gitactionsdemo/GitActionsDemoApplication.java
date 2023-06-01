889ab1f7c6a6b1c63120e39b014d8251014012fb
Pablo-mandalorian <pablotelles935@gmail.com>
2023-05-31 18:04:25 -0600

    first commit

---
+++ src/main/java/com/pablotelles/gitactionsdemo/GitActionsDemoApplication.java
@@ -0,0 +1,13 @@
+package com.pablotelles.gitactionsdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitActionsDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitActionsDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/pablotelles/gitactionsdemo/GitActionsDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pablotelles.gitactionsdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitActionsDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
